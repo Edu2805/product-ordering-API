d4376a93b3e27b6984921f503289f81645c4d90d
Eduardo <eduardoamorim927@gmail.com>
2022-06-26 16:30:43 -0300

    Geração dos arquivo JAR e WAR da aplicação

--- src/main/java/com/springbootexpert/vendas/VendasApplication.java
+++ src/main/java/com/springbootexpert/vendas/VendasApplication.java
@@ -2,9 +2,10 @@ package com.springbootexpert.vendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
