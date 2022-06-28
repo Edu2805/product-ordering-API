8ff4cc9930e68e0eabde70ab1d2eccd0f6ebea38
Eduardo <eduardoamorim927@gmail.com>
2022-06-27 21:59:08 -0300

    Testes unitários na classe ClientService com cobertura de 90% dos métodos

--- src/test/java/com/springbootexpert/vendas/client/ClientServiceTest.java
+++ src/test/java/com/springbootexpert/vendas/client/ClientServiceTest.java
@@ -2,12 +2,14 @@ package com.springbootexpert.vendas.client;
 
 import com.springbootexpert.geradorcpfcnpj.GeraCpfCnpj;
 import com.springbootexpert.vendas.purchase.Purchase;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.ExampleMatcher;
@@ -18,16 +20,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static java.util.UUID.fromString;
+import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class ClientServiceTest {
 
     @InjectMocks
-    private ClientService clientService;
+    private ClientService clientServiceMock;
 
     @InjectMocks
     private GeraCpfCnpj geraCpfCnpjMock;
@@ -40,11 +43,8 @@ class ClientServiceTest {
 
     private Client client;
     private Optional<Client> optionalClient;
-
     private List<Purchase> purchaseList;
-
-    public static final UUID UUID = java.util.UUID.randomUUID();
-
+    public static final UUID UUID = randomUUID();
     public static final String NAME = "Joel";
 
     private void startUser(){
@@ -63,7 +63,7 @@ class ClientServiceTest {
     void whenFilterByIdThenReturnAnClientInstance() {
         when(clientRepositoryMock.findById(UUID)).thenReturn(optionalClient);
 
-        Client response = clientService.getClientById(UUID);
+        Client response = clientServiceMock.getClientById(UUID);
 
         assertNotNull(response);
         assertEquals(Client.class, response.getClass());
@@ -78,11 +78,11 @@ class ClientServiceTest {
         HttpStatus statusMessage = HttpStatus.NOT_FOUND;
 
         String message = assertThrows(ResponseStatusException.class, () -> {
-            clientService.getClientById(java.util.UUID.fromString(uuid));
+            clientServiceMock.getClientById(fromString(uuid));
         }).getReason();
 
         HttpStatus status = assertThrows(ResponseStatusException.class, () -> {
-            clientService.getClientById(java.util.UUID.fromString(uuid));
+            clientServiceMock.getClientById(fromString(uuid));
         }).getStatus();
 
         assertEquals(errorMenssage, message);
@@ -93,7 +93,7 @@ class ClientServiceTest {
     void whenSaveThenReturnSucess() {
         when(clientRepositoryMock.save(any())).thenReturn(client);
 
-        Client response = clientService.save(client);
+        Client response = clientServiceMock.save(client);
 
         assertNotNull(response);
         assertEquals(Client.class, response.getClass());
@@ -107,19 +107,69 @@ class ClientServiceTest {
         when(clientRepositoryMock.findById(UUID)).thenReturn(optionalClient);
 
         try{
-            optionalClient.get().setId(java.util.UUID.fromString(uuid));
-            Client response = clientService.save(client);
+            optionalClient.get().setId(fromString(uuid));
+            Client response = clientServiceMock.save(client);
         } catch (Exception ex){
             assertEquals(DataIntegrityViolationException.class, ex.getClass());
         }
-    }//outro tipo de teste
+    }// fazer validação no método save para não permitir dois ids e dois cpfs
 
     @Test
-    void delete() {
+    void whenFindByIdWithSucessAndDelete() {
+        when(clientRepositoryMock.findById(UUID)).thenReturn(optionalClient);
+
+        optionalClient.get().setId(UUID);
+        var id = optionalClient.get().getId();
+        clientServiceMock.delete(id);
+
+        assertEquals(optionalClient.get().getId(), client.getId());
     }
 
     @Test
-    void update() {
+    void whenFindByIdAndClientDoNotExistsForDelete() {
+        String uuid = "bcf9634c-ab40-47c7-8f5f-45e93735afbf";
+        String errorMenssage = "Cliente não encontrado";
+        HttpStatus statusMessage = HttpStatus.NOT_FOUND;
+
+        String message = assertThrows(ResponseStatusException.class, () -> {
+            clientServiceMock.delete(fromString(uuid));
+        }).getReason();
+
+        HttpStatus status = assertThrows(ResponseStatusException.class, () -> {
+            clientServiceMock.getClientById(fromString(uuid));
+        }).getStatus();
+
+        assertEquals(errorMenssage, message);
+        assertEquals(statusMessage, status);
+    }
+
+    @Test
+    void whenFindByIdWithSucessAndUpdate() {
+        when(clientRepositoryMock.findById(UUID)).thenReturn(optionalClient);
+
+        optionalClient.get().setId(UUID);
+        var id = optionalClient.get().getId();
+        clientServiceMock.update(optionalClient.get(), id);
+
+        assertEquals(optionalClient.get().getId(), client.getId());
+    }
+
+    @Test
+    void whenFindByIdAndClientDoNotExistsForUpdate() {
+        String uuid = "bcf9634c-ab40-47c7-8f5f-45e93735afbf";
+        String errorMenssage = "Cliente não encontrado";
+        HttpStatus statusMessage = HttpStatus.NOT_FOUND;
+
+        String message = assertThrows(ResponseStatusException.class, () -> {
+            clientServiceMock.update(client, fromString(uuid));
+        }).getReason();
+
+        HttpStatus status = assertThrows(ResponseStatusException.class, () -> {
+            clientServiceMock.getClientById(fromString(uuid));
+        }).getStatus();
+
+        assertEquals(errorMenssage, message);
+        assertEquals(statusMessage, status);
     }
 
     @Test
