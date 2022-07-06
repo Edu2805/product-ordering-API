063c31eea613e380e95903efa3542921f21bfea9
Eduardo <eduardoamorim927@gmail.com>
2022-07-05 21:21:23 -0300

    Configurações de deploy

--- src/main/java/com/springbootexpert/vendas/VendasApplication.java
+++ src/main/java/com/springbootexpert/vendas/VendasApplication.java
@@ -3,9 +3,8 @@ package com.springbootexpert.vendas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.springbootexpert.vendas"})
+@SpringBootApplication
 public class VendasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
